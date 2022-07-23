6a2e51281e8b35fc7f483fe5ca5ceefc95b60561
Shivani thamke <thamkeshivani200@gmail.com>
2022-07-23 14:29:25 +0530

    eventvue

---
+++ Event.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <h1>Home Componant</h1>
+    <button v-on:click="test1('button 1 clicked')">1 time click</button>
+    <button v-on:dblclick="test1('button 2 clicked')">2 time click</button>
+    <button v-on:click="getdata()">for count</button>
+    <button v-on:click="getconsole()">clik me</button>
+    <h2>{{count}}</h2>
+</div>
+</template>
+<script>
+
+
+export default {
+    name:"Event",
+    data(){
+        return{
+             count:0
+        }
+    },
+    methods:{
+        getdata()
+        { 
+        
+          this.count=this.count+1;
+
+
+        
+        },
+        test1(data)
+        {
+            alert(data);
+        },
+        getconsole()
+        {
+alert("function called");
+        }
+    }
+}
+</script>
